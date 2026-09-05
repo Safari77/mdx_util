7177aa3d0b4e67e4808c72a0615884bbbdc956b1
Sami Farin <hvtaifwkbgefbaei@gmail.com>
2026-09-05 09:00:51 +0300

    update release.sh

--- release.sh
+++ release.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 # RUSTSEC-2024-0436 paste: rust_icu
-cargo audit --ignore RUSTSEC-2024-0436 && \
+# RUSTSEC-2026-0253 lru: tantivy
+cargo audit --ignore RUSTSEC-2024-0436 --ignore RUSTSEC-2026-0253 -D warnings && \
   cargo clippy && cargo build --release && \
   cargo release patch --no-publish --execute
